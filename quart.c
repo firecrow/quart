fbbb239d17d62a66cc3574a78dca6821a98d8482
firecrow <fire@firecrow.com>
2020-05-12 18:04:55 -0400

    simple parsing of tokens

--- quart.c
+++ quart.c
@@ -11,7 +11,9 @@
 
 enum parse_states {
     QRT_OUT = 0,
-    QRT_SYMBOL
+    QRT_SYMBOL,
+    QRT_PRE_VALUE,
+    QRT_VALUE
 };
 
 int is_alpha(char c){
@@ -26,14 +28,21 @@ int is_alpha_numeric(char c){
     return is_numeric(c) || is_alpha(c);
 }
 
-void emit_token(CtlCounted *name){
-    printf("token:%s\n", ctl_counted_to_cstr(name)); 
+CtlCounted  *emit_token(CtlCounted *name){
+    /*printf("token:%s\n", ctl_counted_to_cstr(name));*/
+    return name;
+}
+
+CtlCounted  *emit_value(CtlCounted *token, CtlCounted *value){
+    printf("pair:%s->%s\n", ctl_counted_to_cstr(token), ctl_counted_to_cstr(value)); 
+    return token;
 }
 
 void parse(char *source){
     char *p = source;
     enum parse_states state = QRT_OUT;
     CtlCounted *shelf = ctl_counted_alloc(NULL, 0);
+    CtlCounted *token = ctl_counted_alloc(NULL, 0);
     if(p == '\0') 
         return;
     do {
@@ -43,13 +52,23 @@ void parse(char *source){
                 state = QRT_SYMBOL;
             }
         }else if(state == QRT_SYMBOL){
-            /*printf("outer symbol d:%d a:%d n:%d c:%c\n",*p, is_alpha(*p),is_alpha_numeric(*p),  *p);*/
             if((shelf->length == 0 && is_alpha(*p)) || is_alpha_numeric(*p)){
-                /*printf("in symbol: %c\n", *p);*/
                 ctl_counted_push(shelf, p, 1);
             }else if(shelf->length > 0){
-                /*printf("leaving symbol: '%c'\n", *p);*/
-                emit_token(shelf); 
+                token = emit_token(shelf); 
+                shelf = ctl_counted_alloc(NULL, 0);
+                state = QRT_PRE_VALUE;
+            }
+        }else if(state == QRT_PRE_VALUE){
+            if(is_alpha_numeric(*p)){
+                state = QRT_VALUE;
+            }
+        }
+        if(state == QRT_VALUE){
+            if(is_alpha_numeric(*p)){
+                ctl_counted_push(shelf, p, 1);
+            }else if(shelf->length > 0){
+                emit_value(token, shelf); 
                 shelf = ctl_counted_alloc(NULL, 0);
                 state = QRT_OUT;
             }
@@ -58,7 +77,7 @@ void parse(char *source){
 }
 
 int main(){
-    char *source = ":y 3\n :run {\n write y\n write x*2 \n}\n :z = run :x 5";
+    char *source = ":y 3\n :run {\n write y\n write x*2 \n}\n :z = run :x 5 ";
     printf("source\n%s\n---------------\n", source);
     parse(source);
 }
