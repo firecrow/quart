e1edef748d88ada931f0d1da2535990d397f1102
firecrow <fire@firecrow.com>
2020-05-18 19:02:45 -0400

    playing around with the formatting

--- quart.c
+++ quart.c
@@ -20,8 +20,8 @@ int main(){
     char *sourcev = ":y 3\n :run {\n write y\n write x*2 \n}\n :z = run :x 5 :y 10; :hightlight 23 :play { y + 3  * { x + 3 + 9 } + 2 }\n ";
     char *source = ":y 3 :x 2; :z 7\n :j 25 ";
     char *s = "\n:run { write x :z 5 write z} :alpha { 2 + {:j 13 :i 0 {7*1 {write use save out run p}}}}";
-    char *z = ":run { write x; write 2*4*y} :juliet 7 + 2 + 4; run :x 2 :y 3;";
-    printf("source\n%s\n---------------\n",s);
+    char *z = ":javalin 0, :run { write x, write 2*4*y, &javalin y} :juliet 7 + 2 + 4, run :x 2 :y 3";
+    printf("source\n%s\n---------------\n",z);
 
     QrtCtx *ctx = build(z);
     CtlAbs *reg = exec(ctx);
