7841a99ea23b181d17fead05c6b3b88dea1e2d64
firecrow <fire@firecrow.com>
2020-05-28 18:35:35 -0400

    exec recursion

--- build.c
+++ build.c
@@ -54,7 +54,7 @@ QrtBlock *pop_block(QrtCtx *ctx){
 }
 
 QrtCell *build_cell(QrtCtx *ctx, QrtCell *actor, QrtCell *args){
-    print_indent(ctx->indent);print_cell(actor);
+    /*print_indent(ctx->indent);print_cell(actor);*/
 
     QrtSymbol *symbol;
     QrtSep *sep;
@@ -72,7 +72,7 @@ QrtCell *build_cell(QrtCtx *ctx, QrtCell *actor, QrtCell *args){
     if((vblock = asQrtBlock(value))){
         if(vblock->type == '{'){
             vblock->branch = break_chain_cell(actor);
-            printf("\x1b[33m branch of %d ", vblock->base.id);print_cell(vblock->branch);
+            /*printf("\x1b[33m branch of %d ", vblock->base.id);print_cell(vblock->branch);*/
             push_block(ctx, vblock);
             ctx->indent += 4;
         }else if(vblock->type == '}'){
@@ -81,7 +81,7 @@ QrtCell *build_cell(QrtCtx *ctx, QrtCell *actor, QrtCell *args){
             if(bblock && bblock->parent_cell){
                 args = break_chain_cell(actor);
                 bblock->parent_cell->next = args;
-                printf("\x1b[33m parent_cell->next of %d ", bblock->base.id);print_cell(args);
+                /*printf("\x1b[33m parent_cell->next of %d ", bblock->base.id);print_cell(args);*/
                 ctx->indent -= 4;
             }
         }else{
@@ -114,7 +114,7 @@ void build(QrtCtx *ctx){
         }
         */
         while(prior && prior != cell){
-             print_indent(ctx->indent);printf("\x1b[36m");print_cell(prior);printf("\x1b[0m");
+            /*print_indent(ctx->indent);printf("\x1b[36m");print_cell(prior);printf("\x1b[0m");*/
             prior = prior->next;
         }
     }
--- exec.c
+++ exec.c
@@ -1,5 +1,19 @@
 QrtCell *exec_cell(QrtCtx *ctx, QrtCell *actor, QrtCell *args){
-    printf("\x1b[34m");print_cell(actor);
+    print_indent(ctx->indent);printf("\x1b[34m");print_cell(actor);
+    QrtBlock *vblock, *closeb, *ablock, *bblock;
+    QrtCell *cell;
+    if((vblock = asQrtBlock(actor->value))){
+        if(vblock->type == 'x'){
+            push_block(ctx, vblock);
+            ctx->indent += 4;
+            cell = vblock->branch;
+            while(cell){
+                cell = exec_cell(ctx, cell, cell->next);
+            }
+            ctx->indent -= 4;
+        }
+
+    }
     return args;
 }
 
@@ -7,10 +21,5 @@ void exec(QrtCtx *ctx){
     QrtCell *cell = ctx->start;   
     while(cell){
         cell = exec_cell(ctx, cell, cell->next);
-        /*
-        if(cell == NULL && ctx->block->parent && ctx->block->parent){
-            cell = ctx->block->parent_cell->next;
-        }
-        */
     }
 }
