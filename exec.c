d7329a7a0ff24405c605f4147f8d6ce70ba5f60d
firecrow <fire@firecrow.com>
2020-05-28 19:00:01 -0400

    .

--- exec.c
+++ exec.c
@@ -1,38 +1,16 @@
-QrtCell *exec_cell(QrtCtx *ctx, QrtCell *actor, QrtCell *args){
-    print_indent(ctx->indent);printf("\x1b[34m");print_cell(actor);
-    QrtBlock *block, *vblock, *closeb, *ablock, *bblock;
-    block = ctx->block;
-    QrtCell *cell; 
-    QrtOpp *opp;
-    if((opp = asQrtOpp(actor->value))){
-        ctx->block->func = opp;
+QrtCell *exec_cell(QrtCtx *ctx, QrtCell *cell){
+    if(!cell) return NULL;
+    print_cell(cell); 
+    QrtBlock *block;
+    if((block = asQrtBlock(cell->value))){
+        exec_cell(ctx, block->branch);  
     }
-    if((vblock = asQrtBlock(actor->value))){
-        if(vblock->type == 'x'){
-            ctx->indent += 4;
-            push_block(ctx, vblock);
-            cell = vblock->branch;
-            while(cell){
-                cell = exec_cell(ctx, cell, cell->next);
-            }
-            pop_block(ctx);
-            ctx->indent -= 4;
-            actor = cell;
-        }
+    if(cell->next){
+        exec_cell(ctx, cell->next);
     }
-    if(block->func){
-        if(block->opp->opp_type == '+'){
-            if(block->req == NULL){
-                block->reg = args; 
-            }
-        }
-    }
-    return args;
+    return cell->next;
 }
 
 void exec(QrtCtx *ctx){
-    QrtCell *cell = ctx->start;   
-    while(cell){
-        cell = exec_cell(ctx, cell, cell->next);
-    }
+    exec_cell(ctx, ctx->start);
 }
