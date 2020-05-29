8364f7f691c0e52fcea042ee64f9a95b7e6f9797
firecrow <fire@firecrow.com>
2020-05-28 23:11:47 -0400

    working nested arithmetic

--- behavior.c
+++ behavior.c
@@ -1,12 +1,17 @@
 /* Copyright 2020 Firecrow Silvernight (fire@firecrow.com) licensed under the MIT License see LICENSE file */
 
 CtlAbs *math_call(QrtBlock *block, CtlAbs *value){
+    print_value(value);
     QrtOpp *opp = block->opp;
     if(!asCtlInt(value)){
-        block->opp = NULL;
-        block->reg = NULL;
         return value;
     }
+
+
+    int regval = block->reg != NULL ? asCtlInt(block->reg)->value : -1;
+    printf("\x1b[36m%d %c %d\x1b[0m\n", regval, opp->opp_type, asCtlInt(value)->value); 
+
+
     if(block->reg == NULL){
         block->reg = value;
     }else{
--- exec.c
+++ exec.c
@@ -4,26 +4,39 @@ CtlAbs *exec_cell(QrtCtx *ctx, QrtCell *cell){
     CtlAbs *value = NULL;
     QrtOpp *opp;
     QrtSep *sep;
+    CtlInt *intvalue;
 
-    while(cell){
+    while(cell->next){
         print_indent(ctx->indent);print_cell(cell); 
         print_indent(ctx->indent);print_block(ctx->block);
         value = cell->value;
-        if((block = asQrtBlock(cell->value)) && block->type != '}'){
-            ctx->block = block;
-            ctx->indent += 2;
-            value = exec_cell(ctx, block->branch);  
-            ctx->indent -= 2;
-            if(ctx->block->parent)
+        if((block = asQrtBlock(cell->value))){
+            if(block->type == '}'){
+                cell = cell->next;
+                continue;
+            }else{
+                ctx->block = block;
+                ctx->indent += 2;
+                value = exec_cell(ctx, block->branch);  
+                printf("-----------: ");print_value(value);
+                ctx->indent -= 2;
                 ctx->block = ctx->block->parent;
+            }
         }else if((opp = asQrtOpp(cell->value))){
             ctx->block->opp = opp;
         }
         if(!opp && ctx->block->opp){
+            printf("sending to opp...\n");
             value = ctx->block->opp->call(ctx->block, value);
+        }
+        if((intvalue = asCtlInt(value))){
+            if(ctx->block->reg == NULL){
+                ctx->block->reg = (CtlAbs *)intvalue;
+            }
         }
         if((sep = asQrtSep(cell->value))){
             ctx->block->reg = NULL;
+            ctx->block->opp = NULL;
         }
         cell = cell->next;
     }
