26647ba8bf8c378893beaebed2a9230733879708
firecrow <fire@firecrow.com>
2020-06-01 00:37:50 -0400

    moving the placement

--- exec.c
+++ exec.c
@@ -29,9 +29,6 @@ CtlAbs *exec_value(QrtCtx *ctx, CtlAbs *value){
     QrtSymbol *symbol;
     CtlInt *intvalue;
     print_indent(ctx->indent);print_block(ctx->block);
-    if(asQrtOpp(value)){
-        opp = push_opp(ctx->block, asQrtOpp(value));
-    }
     if((symbol = asQrtSymbol(value))){
         if(symbol->type == 'x'){
             value = ctl_tree_get(ctx->block->namespace, (CtlAbs *)symbol->name);
@@ -52,6 +49,9 @@ CtlAbs *exec_value(QrtCtx *ctx, CtlAbs *value){
             ctx->indent -= 2;
             ctx->block = ctx->block->parent;
         }
+    }
+    if(asQrtOpp(value)){
+        opp = push_opp(ctx->block, asQrtOpp(value));
     }
     if(!opp && ctx->block->opp){
         value = ctx->block->opp->call(ctx, value);
